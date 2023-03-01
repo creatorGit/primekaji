65270784d8562ca6301d91080ed07ebe9ab5aeca
creatorGit <web.passion.dev@gmail.com>
2023-03-01 17:08:55 +0900

    Add top20, top27, and top37

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,3 +98,170 @@ function pushHideButton2() {
     btnEye.className = "fa fa-eye-slash toggle-eyes";
   }
 }
+
+
+function openTab(evt, tabname) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabname).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+class Pagination {
+  constructor(dotsSelector) {
+    this.dotsSelector = dotsSelector;
+
+    this.maxLinks = 5;
+    this.maxOffset = Math.ceil(this.maxLinks / 2);
+
+    document.querySelector(this.dotsSelector).innerHTML = "";
+  }
+
+  generate(currentSlide, countSlides) {
+    this.currentSlide = currentSlide + 1;
+    this.countSlides = countSlides;
+
+    this.offset_left = this.currentSlide > this.maxOffset;
+    this.offset_right = this.currentSlide <= this.countSlides - this.maxOffset;
+
+    if (this.countSlides > this.maxLinks) {
+      if (this.currentSlide <= Math.floor(this.countSlides / 2)) {
+        this.generateLeftPosition();
+      } else if (this.currentSlide > Math.floor(this.countSlides / 2)) {
+        this.generateRightPosition();
+      }
+    } else {
+      this.generateDefault();
+    }
+  }
+
+  generateDot(i, text, className = false, current = false) {
+    var dot = document.createElement("li");
+
+    if (className) {
+      dot.classList.add(className);
+    }
+
+    if (i != "" && this.currentSlide == i) {
+      dot.classList.add("active");
+    }
+
+    dot.id = i - 1;
+    dot.innerText = text;
+
+    document.querySelector(this.dotsSelector).appendChild(dot);
+  }
+
+  generateLeftPosition() {
+    if (this.currentSlide != 1) {
+      for (
+        var i = this.currentSlide - 1;
+        i < this.currentSlide - 1 + this.maxOffset;
+        i++
+      ) {
+        this.generateDot(i, i, "dot");
+      }
+      this.generateDot("", "...");
+      this.generateDot(this.countSlides, this.countSlides, "dot");
+    } else {
+      for (
+        var i = this.currentSlide;
+        i < this.currentSlide + this.maxOffset;
+        i++
+      ) {
+        this.generateDot(i, i, "dot");
+      }
+      this.generateDot("", "···");
+      this.generateDot(this.countSlides, this.countSlides, "dot");
+    }
+  }
+
+  generateRightPosition() {
+    if (this.currentSlide != this.countSlides) {
+      this.generateDot(1, 1, "dot");
+      this.generateDot("", "···");
+      for (
+        var i = this.currentSlide - 1;
+        i < this.currentSlide - 1 + this.maxOffset;
+        i++
+      ) {
+        this.generateDot(i, i, "dot");
+      }
+    } else {
+      this.generateDot(1, 1, "dot");
+      this.generateDot("", "···");
+      for (
+        var i = this.countSlides + 1 - this.maxOffset;
+        i <= this.countSlides;
+        i++
+      ) {
+        this.generateDot(i, i, "dot");
+      }
+    }
+  }
+
+  generateDefault() {
+    for (var i = 1; i < this.countSlides + 1; i++) {
+      this.generateDot(i, i, "dot");
+    }
+  }
+}
+
+$(document).ready(function() {
+  if ($("#defaultOpen")) {
+    document.getElementById("defaultOpen").click();
+    $('.tab').change(function(event){
+      openTab(event, $(this).val())
+    })
+  }
+
+  if ($("#g2-slider")) {
+    var g2Slider = $("#g2-slider");
+
+    function addSliderListeners() {
+      var dots = document.querySelectorAll(".dot");
+      dots.forEach(function (el) {
+        el.addEventListener("click", function () {
+          g2Slider.slick("slickGoTo", el.id);
+        });
+      });
+    }
+
+    g2Slider.on("init", function (event, slick) {
+      var pag = new Pagination("#g2-slider-pagination-list");
+      pag.generate(slick.currentSlide, slick.slideCount);
+      addSliderListeners();
+    });
+
+    g2Slider.on(
+      "beforeChange",
+      function (event, slick, currentSlide, nextSlide) {
+        var pag = new Pagination("#g2-slider-pagination-list");
+        pag.generate(nextSlide, slick.slideCount);
+
+        addSliderListeners();
+      }
+    );
+
+    /* slider initialization */
+    g2Slider.slick({
+      infinite: true,
+      adaptiveHeight: true,
+      fade: true,
+      speed: 500,
+      cssEase: "linear",
+      prevArrow: $(".g2-slider-pagination .prev"),
+      nextArrow: $(".g2-slider-pagination .next")
+    });
+  }
+});
